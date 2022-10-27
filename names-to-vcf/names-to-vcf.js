2e5ed300beb05fcc7170aacc51af7c9d0fcf051c
Pavol Hejny <me@pavolhejny.com>
2022-10-27 14:31:46 +0200

    TODOs

--- names-to-vcf/names-to-vcf.js
+++ names-to-vcf/names-to-vcf.js
@@ -80,3 +80,4 @@ function forImmediate() {
 // TODO: !!! Same naming convertNamesToVcard everyhere
 // TODO: !!! Same HTMLInputElement readonly everyhere
 // TODO: !!! Create for all tools menu OR backink to main README
+// TODO: !!! Reflect input into URL after the hash part #
