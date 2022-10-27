65c9caf42104c61ba734b1948af1ef712e736a32
Pavol Hejny <me@pavolhejny.com>
2022-10-27 14:17:41 +0200

    Names to VCF working

--- names-to-vcf/names-to-vcf.ts
+++ names-to-vcf/names-to-vcf.ts
@@ -1,12 +1,42 @@
-
 const namesElement = document.getElementById("names") as HTMLTextAreaElement;
-const vcardElement = document.getElementById("vcard");
+const noteElement = document.getElementById("note") as HTMLInputElement;
+const vcardElement = document.getElementById("vcard") as HTMLInputElement;
 
+namesElement.addEventListener("keydown", convertNamesToVcard);
+namesElement.addEventListener("change", convertNamesToVcard);
+noteElement.addEventListener("keydown", convertNamesToVcard);
+noteElement.addEventListener("change", convertNamesToVcard);
 
-vcardElement.addEventListener("keydown", convertNamesToVcard);
-vcardElement.addEventListener("change", convertNamesToVcard);
 convertNamesToVcard();
 
-function convertNamesToVcard() {
-  vcardElement.innerHTML = namesElement.value;
+async function convertNamesToVcard() {
+  await forImmediate();
+  const note = noteElement.value.trim() || null;
+  vcardElement.innerHTML = namesElement.value
+    .split("\n")
+    .flatMap((name) => [
+      `BEGIN:VCARD`,
+      `VERSION:3.0`,
+      `FN:${name}`,
+      note ? `NOTE:${note}` : null,
+      `END:VCARD`,
+    ])
+    .filter((row) => row != null)
+    .join("\n");
 }
+
+function forImmediate(): Promise<void> {
+  // Note: Not using setImmediate because it is non-standard feature only in browser window
+  //       @see https://developer.mozilla.org/en-US/docs/Web/API/Window/setImmediate
+
+  return new Promise((resolve) => {
+      setTimeout(() => {
+          resolve();
+      }, 1);
+  });
+}
+
+
+// !!! Same async+forImmediate everyhere
+// !!! Same naming convertNamesToVcard everyhere
+// !!! Same HTMLInputElement readonly everyhere
